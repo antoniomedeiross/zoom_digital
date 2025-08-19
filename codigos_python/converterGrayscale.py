72d2f198380476cff0f86b2054eb377b15931872
Antonio <jantonioaparecidoj333@gmail.com>
2025-08-18 23:19:35 -0300

    sei la

--- codigos_python/converterGrayscale.py
+++ codigos_python/converterGrayscale.py
@@ -2,9 +2,9 @@ from PIL import Image
 import numpy as np
 
 # --- CONFIGURAÇÕES ---
-IMG_PATH = r"C:\\Users\\felip\\Documents\\uefs\\hardware\\sd\\zoom_digital\\images\\frieren-orig.jpeg"
+IMG_PATH = r"C:\\Users\Antonio\\OneDrive\Documentos\\universidade\\PBL\\hardware\\SD\\p1\\codigos\\images\\frieren-orig.jpeg"
 WIDTH, HEIGHT = 320, 240   # resolução da VGA
-OUTPUT_MIF = "image.mif"
+OUTPUT_HEX = "image.hex"
 
 # --- ABRIR E CONVERTER A IMAGEM ---
 img = Image.open(IMG_PATH).convert("L")  # grayscale 8 bits
@@ -14,18 +14,10 @@ pixels = np.array(img, dtype=np.uint8)   # matriz (H x W)
 # --- FLATTEN EM UMA LISTA ---
 pixel_list = pixels.flatten()  # vira 1D
 
-# --- GERAR O CABEÇALHO DO .MIF ---
-with open(OUTPUT_MIF, "w") as f:
-    f.write(f"WIDTH=8;\n")                 # cada pixel tem 8 bits
-    f.write(f"DEPTH={WIDTH*HEIGHT};\n")    # total de pixels
-    f.write("ADDRESS_RADIX=DEC;\n")        # endereços em decimal
-    f.write("DATA_RADIX=HEX;\n")           # dados em hexadecimal
-    f.write("CONTENT BEGIN\n")
+# --- GERAR O ARQUIVO .HEX ---
+with open(OUTPUT_HEX, "w") as f:
+    for val in pixel_list:
+        f.write(f"{val:02X}\n")  # escreve cada pixel em hexadecimal
 
-    # escreve cada pixel no .mif
-    for addr, val in enumerate(pixel_list):
-        f.write(f"    {addr} : {val:02X};\n")  # endereço : valor_hex;
+print(f"Arquivo .hex gerado: {OUTPUT_HEX}")
 
-    f.write("END;\n")
-
-print(f"Arquivo .mif gerado: {OUTPUT_MIF}")
